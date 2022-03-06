b33945652cd0e7f9735adb67e466b13a582bcdf9
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2022-03-06 23:17:31 +0700

    add func list queue

---
+++ example/sqs/list_queues.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	awsInternal "github.com/galihsatriawan/trial-sns-sqs/internal/pkg/aws"
+	"github.com/galihsatriawan/trial-sns-sqs/utils"
+)
+
+var config utils.Config
+
+var sqsClient awsInternal.SQSClient
+
+func init() {
+	var err error
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	configPath := filepath.Join(basepath, "../../")
+
+	config, err = utils.LoadConfig(configPath)
+	if err != nil {
+		panic(err)
+	}
+	sqsClient = awsInternal.NewSQS(config)
+}
+func main() {
+	ctx := context.Background()
+	urls, err := sqsClient.ListQueues(ctx)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range urls {
+		fmt.Println(*v)
+	}
+}
